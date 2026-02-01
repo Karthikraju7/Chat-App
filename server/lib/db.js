ff54c996d79965605e815f561a02407b5a2ebbf4
Karthik <karthikraju4897@gmail.com>
2026-02-01 12:23:22 +0530

    fix: database connection

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,13 +1,25 @@
 import mongoose from "mongoose";
 
-// To connect with MongoDB database
-export const connectDB = async () =>{
-    try{
-        mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        mongoose.connection.on("error", (err) =>console.log("MongoDB Error:", err));
-        mongoose.connection.on("disconnected", () =>console.log("Databse Disconnected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
-    } catch (error){
-        console.log(error);
-    }
-}
+export const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+
+    mongoose.connection.on("error", (err) =>
+      console.log("MongoDB Error:", err)
+    );
+
+    mongoose.connection.on("disconnected", () =>
+      console.log("Database Disconnected")
+    );
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+        family: 4, 
+        serverSelectionTimeoutMS: 10000,
+        });
+
+  } catch (error) {
+    console.log("Connection Logic Error:", error);
+  }
+};
